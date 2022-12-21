6f804e87fa20721aecf972595f62c0a0db3a692f
patrickday7 <120026345+patrickday7@users.noreply.github.com>
2022-12-21 12:14:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
+actions/checkout@v2, actions/setup-node@v1
